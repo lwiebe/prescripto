73d30020158edebf46072f564fb5088fa18354d4
Lian Wiebe <lnwiebe05@gmail.com>
2024-10-01 08:06:28 +0200

    about fonish

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const About = () => {
   return (
     <div>
-      
+
       <div className='text-center text-2xl pt-10 text-gray-500'>
         <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
       </div>
@@ -23,18 +23,18 @@ const About = () => {
         <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span> </p>
       </div>
 
-      <div>
-        <div>
-          <b></b>
-          <p></p>
+      <div className='flex flexx-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
         </div>
-        <div>
-          <b></b>
-          <p></p>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience:</b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
         </div>
-        <div>
-          <b></b>
-          <p></p>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
         </div>
       </div>
 
